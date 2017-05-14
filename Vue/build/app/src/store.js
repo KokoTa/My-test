946f3b99f7d8cecb86130ac80d9eff05b9cef9f4
KokoTa <584847514@qq.com>
2017-05-14 23:46:49 +0800

    :construction: Vuex
    学习Vuex中， 注意点写在注释里了， 同时要注意vue-simple模板的.babelrc需要更改配置为stage-3,
    否则无法使用扩展运算符

--- Vue/build/app/src/store.js
+++ Vue/build/app/src/store.js
@@ -6,16 +6,24 @@ const state = { // 状态对象， 存放公共数据
 	count: 4
 }
 
-const mutations = { // 状态方法， 改变数据的方法
-	increase (state) {
-		state.count++
+const mutations = { // 触发状态， 注意方法都是同步的
+	increase (state, n) {
+		state.count += n.a
 	},
 	decrease (state) {
 		state.count--
 	}
 }
 
+const getters = { // 计算状态， 不改变状态对象， PS：computed不要用箭头函数， this会出错
+	count: function (state) {
+		return state.count + 100
+	}
+}
+
+//输出实例， 被组件使用
 export default new Vuex.Store({
 	state,
-	mutations
+	mutations,
+	getters
 })
--- Vue/build/app/src/vuex.vue
+++ Vue/build/app/src/vuex.vue
@@ -1,14 +1,14 @@
 <template>
 	<div id="app">
 		<h1>Hello</h1>
-		<p>{{ count }} - {{ count2 }} - {{ count3 }}</p>
-		<button @click='$store.commit("increase")'>+</button>
-		<button @click='$store.commit("decrease")'>-</button>
+		<p>{{ count }} - {{ count2 }} - {{ count3 }} - {{ count4 }}</p>
+		<button @click='increase({ a: 10 })'>+</button>
+		<button @click='decrease'>-</button>
 	</div>
 </template>
 
 <script>
-	import { mapState } from 'vuex'
+	import { mapState, mapGetters, mapMutations } from 'vuex'
 	export default {
 		data () {
 			return {
@@ -20,12 +20,26 @@
 		// 		return this.$store.state.count + 1 // 一直这样引入太麻烦，引入mapState简化操作
 		// 	}
 		// }
-		computed: mapState({
-			count: state => state.count, // 写法一
-			count2: 'count', // 写法二， 'count' 等同于 `state => state.count`
-			count3 (state) { // 写法三， 当需要运用到局部状态时需使用常规函数
-				return state.count + this.num
-			}
-		})
+		// mapState映射状态， mapGetters映射派生状态， mapMutations映射方法， 其中前两个不改变状态对象
+		// 写法一
+		// 写法二， 'count' 等同于 `state => state.count`
+		// 写法三， 当需要运用到局部状态时需使用常规函数
+		// ...{} 混入
+		computed: {
+			...mapState({
+				count: state => state.count, 
+				count2: 'count', 
+				count3 (state) { 
+					return state.count + this.num
+				}
+			}),
+			...mapGetters({
+				count4: 'count'
+			})
+		},
+		methods: mapMutations([ // 映射方法, 注意这个是数组， 当然也可以用对象形式写
+			'increase',
+			'decrease'
+		])
 	}
 </script>
